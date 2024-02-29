13ed21b4b87e588e948d83921b4de0126c672e84
AnnVynnychenkoa <avinnichenko93@gmail.com>
2024-02-29 17:13:21 +0200

    layouts

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { headers } from 'next/headers';
+import AddCompanyButton from './components/add-company-button';
+import ServerComponent from './components/server-component';
+import ClientComponent from './components/client-component';
+import ServerComponentCopy from './components/server-component copy';
+
+export default function Home() {
+  console.log(headers());
+  return (
+    <main>
+      <h1 className="text-xl">Home page {new Date().toLocaleTimeString()}</h1>
+      <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
+    </main>
+  );
+}
